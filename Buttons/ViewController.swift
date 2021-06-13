8da032e110f021fb3587460848402f7f1d4336bb
Kirill Ki <kirillkalapov@gmail.com>
2021-06-13 22:55:59 +0200

    Updated

--- Buttons/ViewController.swift
+++ Buttons/ViewController.swift
@@ -13,13 +13,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var button: UIButton!
     
+    @IBOutlet var actionButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
        label.isHidden = true
         button.backgroundColor = .systemGray5
-      button.setTitle("Push", for: .normal)
-//        button.setTitleColor(.black, for: .normal)
+      button.setTitle("Clear", for: .normal)
+        button.isHidden = true
+        
+        for button in actionButtons {
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+//        button.setTitle("Clear", for: .normal)
+//        button.setTitleColor(.white, for: .normal)
+//        button.backgroundColor = .red
         
     }
 
@@ -27,15 +37,28 @@ class ViewController: UIViewController {
      //   label.isHidden = false
         // label.text = "Hello!"
         
-        if label.isHidden {
-            label.text = "Hello!"
-            label.isHidden = false
-            button.setTitle("Back", for: .normal)
-        } else {
+//            if label.isHidden {
+//                label.text = "Hello!"
+//                label.isHidden = false
+//                button.setTitle("Back", for: .normal)
+//                //button.backgroundColor = .blue
+//            } else {
+//                label.isHidden = true
+//                button.setTitle("Push", for: .normal)
+//                //button.backgroundColor = .red
+//            }
+//
+        label.isHidden = false
+        button.isHidden = false
+        if sender.tag == 0 {
+            label.text = "Hello world!"
+        } else if sender.tag == 1 {
+            label.text = "Hi there!"
+            label.textColor = .blue
+        } else if sender.tag == 2 {
             label.isHidden = true
-            button.setTitle("Push", for: .normal)
+            button.isHidden = true
         }
-        
         
     }
     
